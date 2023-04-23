f1c6c6a271f265300e05e2215316ed5aa5863216
Andrew Klavekoske <aklavo@comcast.net>
2023-04-23 16:07:39 -0600

    add masd methods to relevant classes

--- compenents.py
+++ compenents.py
@@ -11,6 +11,10 @@ class SolarPanel:
     self.Q_in = Q_in
     self.Q_out = Q_out
     
+  def mass_fulid(self, rho_fuild, pipe_diameter):
+    # the volume of water in the panel is approximated to be panel area multiplied by pipe diamter
+    return self.area*pipe_diameter*rho_fuild
+    
 class Pump:
   def __init__(self, temp_in, temp_out, flow_in, flow_out, Q_in, Q_out):
     self.temp_in = temp_in
@@ -29,7 +33,10 @@ class StorageTank:
     self.flow_out = flow_out
     self.Q_in = Q_in
     self.Q_out = Q_out
-
+    
+  def mass_fulid(self, rho_fuild):
+    return self.volume*rho_fuild
+  
 class Pipe:
   def __init__(self, length, diameter, temp_in, temp_out, flow_in, flow_out, Q_in, Q_out):
     self.length = length
@@ -41,6 +48,9 @@ class Pipe:
     self.Q_in = Q_in
     self.Q_out = Q_out
     
-    def volume(self):
-      return math.pi*self.length*(self.diameter/2)**2
+  def volume(self):
+    return math.pi*self.length*(self.diameter/2)**2
+  
+  def mass_fulid(self, rho_fuild):
+    return self.volume()*rho_fuild
 
