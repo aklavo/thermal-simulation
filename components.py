e6abecac17df4efa96f5c623fd887a32bd907df8
Andrew Klavekoske <andrewklavo@gmail.com>
2024-07-14 20:18:29 -0600

    restruture all classes, fluids, get volume from the container they're in

---
+++ components.py
@@ -0,0 +1,135 @@
+import math
+from abc import ABC, abstractmethod
+#################################################
+# Q = m * c * dT
+# T_f = ((m1*T1)+(m2*T2))/(m1 + m2)
+#
+# Conduction: Q = k * A * (T_hot - T_cold)^(t/d)
+# Convection: Q = h * A * (T_hot - T_cold)
+# Radiation: Q = sig * A*  (T_hot - T_cold)^4
+#################################################
+
+
+# ------------------------------- Fluids -------------------------------
+class Fluid:
+  def __init__(self,density, specific_heat, temperature=20, container=None):
+    self.density = density
+    self.specific_heat = specific_heat
+    self.temperature = temperature # initial temperature of water 20°C
+    self.container = container # container object if fluid is in a container
+
+
+  def volume(self):
+    return self.container.volume() if self.container else 0
+  
+  def mass(self):
+    return self.density*self.volume()
+  
+  def to_kelvin(self):
+    return self.temperature + 273.15
+  
+  def add_container(self, container):
+    self.container = container
+  
+  # T2 = T1 + Q/mc
+  def add_energy(self, energy):
+    self.temperature += energy/(self.specific_heat*self.mass())
+
+  # Heat lossed by fluid_1 + Heat gained by fluid_2 = 0
+  def mix_with(self, fluid):
+    mass_1 = self.mass()
+    mass_2 = fluid.mass()
+    mass_3 = mass_1 + mass_2
+    temp_1 = self.temperature
+    temp_2 = fluid.temperature
+
+    # update both fluid temperature to final temperatre
+    self.temperature = ((mass_1*temp_1)+(mass_2*temp_2))/(mass_3)  
+    fluid.temperature = self.temperature  
+
+# ------------------------------- Surfaces -------------------------------
+class Material:
+  def __init__(self, heat_transfer_coefficient, surface_temperature, thickness):
+    self.heat_transfer_coefficient = heat_transfer_coefficient
+    self.surface_temperature = surface_temperature 
+    self.thickness = thickness
+
+# ------------------------------- The Sun -------------------------------
+class Sun:
+  def __init__(self):
+    self.irradiance = 0 # intal irradiance in W/m^2 or J/s*m^2
+
+  def energy(self, time):
+    return self.irradiance*time
+  
+# ------------------------------- Containers -------------------------------    
+class Container(ABC):
+  def __init__(self, fluid, material):
+    self.fluid = fluid
+    self.material = material
+
+  @abstractmethod
+  def volume(self):
+    pass
+
+  @abstractmethod
+  def surface_area(self):
+    pass
+    
+  def conduction_loss(self, material, fluid, time):
+    temp_1 = self.fluid.temperature
+    temp_2 = fluid.temperature
+    thickness = material.thickness
+    heat_transfer_coefficient = material.heat_transfer_coefficient
+    area = self.surface_area()
+
+    heat_energy = heat_transfer_coefficient*area*(temp_1-temp_2)**(time/thickness)
+
+    return heat_energy
+  
+# ------------------------------- Solar Panel -------------------------------
+class SolarPanel(Container):
+  def __init__(self, fluid, material, length, width, hieght):
+    super().__init__(fluid, material)
+    self.length = length
+    self.width = width
+    self.height = hieght
+    self.efficiency = 0.7 # % of sunlight coverted to heat <-- this is a heat transfer bandaid for now
+
+  def volume(self):
+    return self.length * self.width * self.height
+  
+  def surface_area(self):
+    top_bottom = 2*(self.length * self.width)
+    left_right = 2*(self.length * self.height)
+    front_back = 2*(self.width * self.height)
+    return top_bottom + left_right + front_back
+    
+
+ # ------------------------------- Water Tank -------------------------------   
+class Tank(Container):
+  def __init__(self, fluid, material, radius, height):
+    super().__init__(fluid, material)
+    self.radius = radius
+    self.height = height
+
+  def volume(self):
+    return math.pi*self.height*self.radius**2
+  
+  def surface_area(self):
+    return (2*math.pi*self.radius**2)+(2*math.pi*self.radius*self.height)
+    
+# ------------------------------- Pipe -------------------------------   
+class Pipe(Container):
+  def __init__(self, fluid, material, radius, length):
+    super().__init__(fluid, material)
+    self.radius = radius
+    self.length = length
+
+  def volume(self):
+    return math.pi*self.length*self.radius**2
+  
+  def surface_area(self):
+    return (2*math.pi*self.radius*self.length)
+
+
--- main.py
+++ main.py
@@ -2,128 +2,148 @@ import matplotlib
 
 matplotlib.use("TkAgg") # To show plots in Ubuntu
 import matplotlib.pyplot as plt
-import compenents as comps
+import components as comps
 import inputs
 
-# System constants
-flow_rate = 0.005 # [kg/s]
-water_density = 100 # density of water at 4°C [kg/m^3]
-water_specific_heat = 4184 # specific heat of water at 20°C [J/kg°C]
-panel_volume = 1.0 # [m^3]
-tank_volume = 3.0 # [m^3]
-sigma = 5.670367e-8 # Stefan-Boltzmann constant [W/m^2*K^4]
 
-# System intial conditions
-oa_temp = 26.6667 # [°C] Outside ambient air temperature 80°F
-zone_temp = 21.111 # [°C] Inside ambient air temperature 70°F
+def main():
+    # System constants
+    flow_rate = 0.005 # [kg/s]
+    water_density = 100 # density of water at 4°C [kg/m^3]
+    water_specific_heat = 4184 # specific heat of water at 20°C [J/kg°C]
+    panel_length = 2 # [m]
+    panel_width = 1 # [m]
+    panel_hieght = 0.1 # [m]
+    tank_radius = 0.5 # [m]
+    tank_height = 2 # [m]
+    sigma = 5.670367e-8 # Stefan-Boltzmann constant [W/m^2*K^4]
+    k_stainless_steal = 17 # Thermal conductivity of stainless steel [W/m*K]
+    k_glass = 1 # Thermal conductivity of glass [W/m*K]
+    k_copper = 401 # Thermal conductivity of copper [W/m*K]
 
-# Components
-panel_water = comps.Fluid(panel_volume, water_density, water_specific_heat, oa_temp)
-tank_water = comps.Fluid(tank_volume, water_density, water_specific_heat, zone_temp) 
-sun = comps.Sun()
-panel = comps.SolarPanel(panel_water)
-tank = comps.Tank(tank_water)
+    # System intial conditions
+    oa_temp = 26.6667 # [°C] Outside ambient air temperature 80°F
+    zone_temp = 21.111 # [°C] Inside ambient air temperature 70°F
 
-# Lists to store simulation results
-panel_temperatures = []
-tank_temperatures = []
-solar_energy = []
+    # Components
+    sun = comps.Sun()
+    panel_water = comps.Fluid(water_density, water_specific_heat, oa_temp)
+    tank_water = comps.Fluid(water_density, water_specific_heat, zone_temp) 
+    tank_stainless_steal = comps.Material(k_stainless_steal, zone_temp, 0.03)
+    panel_glass = comps.Material(k_glass, oa_temp, 0.01)
+    copper_pipe = comps.Material(k_copper, zone_temp, 0.005)
 
-# Weather parameters
-year = '2022'
-lat = '39.8818'
-lon = '-105.0552'
-interval = '5'
-attributes = 'ghi,clearsky_ghi,air_temperature'
-sim_step = '5min'
-weather_df = inputs.get_weather_data(lat, lon, year, interval, attributes,sim_step) # 5min data
+    panel = comps.SolarPanel(panel_water, panel_glass, panel_length, panel_width, panel_hieght)
+    tank = comps.Tank(tank_water, tank_stainless_steal, tank_radius, tank_height)
 
-# Simulation parameters
-start = '2022-07-01 00:00:00'
-end = '2022-07-01 23:55:00'
-weather_df = weather_df.loc[start:end]
-sim_length = len(weather_df)
-sim_step_seconds = (weather_df.index[1]-weather_df.index[0]).total_seconds() # [s]
-clouds = 1 # 1 = use GHI, -1 = use Clearsky GHI, 0 = no sun
+    # Put Water in containers
+    panel.fluid.add_container(panel)
+    tank.fluid.add_container(tank)
 
-# Simulation loop in seconds
-print(f"Starting simulation at {sim_step} intervals...")
-for i in range(sim_length):
-    print(f"Time: {weather_df.index[i]}")
-    print(f"Panel Fluid Temp before heat transfer: {panel.fluid.temperature:.3f}")
-    print(f"Tank Fluid Temp before heat transfer: {tank.fluid.temperature:.3f}")
-    # Store sun energy
-    if clouds == 1:
-        sun.irradiance = weather_df.iloc[i]['GHI']
-    elif clouds == -1:
-        sun.irradiance = weather_df.iloc[i]['Clearsky GHI']
-    else:
-        sun.irradiance = 0
+    # Lists to store simulation results
+    panel_temperatures = []
+    tank_temperatures = []
+    solar_energy = []
 
-    solar_energy.append(sun.irradiance)
+    # Weather parameters
+    year = '2022'
+    lat = '39.8818'
+    lon = '-105.0552'
+    interval = '5'
+    attributes = 'ghi,clearsky_ghi,air_temperature'
+    sim_step = '5min'
+    weather_df = inputs.get_weather_data(lat, lon, year, interval, attributes,sim_step) # 5min data
 
-    # Solar energy into the panel
-    energy_to_panel = sun.energy(sim_step_seconds)*panel.efficiency
-    panel.fluid.add_energy(energy_to_panel, panel.fluid.mass(panel.volume))
-    
-    # heat loss
-    #panel.fluid.temperature -= panel.heat_loss()
-    #tank.fluid.temperature -= tank.heat_loss()
+    # Simulation parameters
+    start = '2022-07-01 00:00:00'
+    end = '2022-07-03 23:55:00'
+    weather_df = weather_df.loc[start:end]
+    sim_length = len(weather_df)
+    sim_step_seconds = (weather_df.index[1]-weather_df.index[0]).total_seconds() # [s]
+    clouds = 1 # 1 = use GHI, -1 = use Clearsky GHI, 0 = no sun
 
-    # Piping/moving the fluid
-    supply_volume = (flow_rate * sim_step_seconds) / panel.fluid.density
-    supply_temp = panel.fluid.temperature
-    supply_flow = comps.Fluid(supply_volume, water_density, water_specific_heat, supply_temp)
+    # Simulation loop in seconds
+    print(f"Starting simulation at {sim_step} intervals...")
+    for i in range(sim_length):
+        print(f"Time: {weather_df.index[i]}")
+        print(f"Panel Fluid Temp before heat transfer: {panel.fluid.temperature:.3f}")
+        print(f"Tank Fluid Temp before heat transfer: {tank.fluid.temperature:.3f}")
+        # Store sun energy
+        if clouds == 1:
+            sun.irradiance = weather_df.iloc[i]['GHI']
+        elif clouds == -1:
+            sun.irradiance = weather_df.iloc[i]['Clearsky GHI']
+        else:
+            sun.irradiance = 0
 
-    return_volume = (flow_rate * sim_step_seconds) / tank.fluid.density
-    return_temp = tank.fluid.temperature
-    return_flow = comps.Fluid(return_volume, water_density, water_specific_heat, return_temp)
+        solar_energy.append(sun.irradiance)
 
-    # Update Tank and Panel temperatures
-    tank.fluid.mix_with(supply_flow)
-    print(f"Tank Fluid Temp after heat transfer: {tank.fluid.temperature:.3f}")
+        # Solar energy into the panel
+        energy_to_panel = sun.energy(sim_step_seconds)*panel.efficiency
+        panel.fluid.add_energy(energy_to_panel)
+        
+        # heat loss
+        #panel.fluid.temperature -= panel.heat_loss()
+        #tank.fluid.temperature -= tank.heat_loss()
 
-    panel.fluid.mix_with(return_flow)
-    print(f"Panel Fluid Temp after heat transfer: {panel.fluid.temperature:.3f}")
+        # Piping/moving the fluid
+        supply_temp = panel.fluid.temperature
+        supply_hw = comps.Fluid(water_density, water_specific_heat, supply_temp)
+        supply_pipe = comps.Pipe(supply_hw, copper_pipe, 0.02, 5)
+        supply_pipe.fluid.add_container(supply_pipe)
 
-    # store temperatures and energies
-    panel_temperatures.append(panel.fluid.temperature)
-    tank_temperatures.append(tank.fluid.temperature)
-    print("---------------------------------------")
-    
-    
-print("Simulation complete!")
+        return_temp = tank.fluid.temperature
+        return_hw = comps.Fluid(water_density, water_specific_heat, return_temp)
+        return_pipe = comps.Pipe(return_hw, copper_pipe, 0.02, 5)
+        return_pipe.fluid.add_container(return_pipe)
+        
+        # Update Tank and Panel temperatures
+        tank.fluid.mix_with(supply_hw)
+        print(f"Tank Fluid Temp after heat transfer: {tank.fluid.temperature:.3f}")
 
-############ Create the plot ##############
-x = weather_df.index  # time
-panel_color = "red"
-tank_color = "blue"
-sun_color = "orange"
-oat_color = "green"
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
+        panel.fluid.mix_with(return_hw)
+        print(f"Panel Fluid Temp after heat transfer: {panel.fluid.temperature:.3f}")
 
-# Temperature plot
-ax1.plot(x, panel_temperatures, label="Panel Fluid Temp", color=panel_color)
-ax1.plot(x, tank_temperatures, label="Tank Fluid Temp", color=tank_color)
-ax1.set_title("Solar Water Heating Simulation")
-ax1.set_ylabel("Temperature (°C)")
-ax1.tick_params(axis="y")
-ax1.legend(loc="upper right")
+        # store temperatures and energies
+        panel_temperatures.append(panel.fluid.temperature)
+        tank_temperatures.append(tank.fluid.temperature)
+        print("---------------------------------------")
+        
+        
+    print("Simulation complete!")
 
-# Irradiance plot (right y-axis for ax1)
-ax1_twin = ax1.twinx()
-ax1_twin.set_ylabel("Irradiance (W/m^2)", color=sun_color)
-ax1_twin.plot(x, solar_energy, label="Irradiance", color=sun_color)
-ax1_twin.tick_params(axis="y", labelcolor=sun_color)
-ax1_twin.legend(loc="upper left")
+    ############ Create the plot ##############
+    x = weather_df.index  # time
+    panel_color = "red"
+    tank_color = "blue"
+    sun_color = "orange"
+    oat_color = "green"
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
 
-# Other plot
-ax2.plot(x, weather_df["Temperature"], label="Outside Air Temp", color=oat_color)
-ax2.set_xlabel("Time")
-ax2.set_ylabel("Temperature (°C)")
-ax2.legend(loc="upper right")
+    # Temperature plot
+    ax1.plot(x, panel_temperatures, label="Panel Fluid Temp", color=panel_color)
+    ax1.plot(x, tank_temperatures, label="Tank Fluid Temp", color=tank_color)
+    ax1.set_title("Solar Water Heating Simulation")
+    ax1.set_ylabel("Temperature (°C)")
+    ax1.tick_params(axis="y")
+    ax1.legend(loc="upper right")
 
-plt.tight_layout()
-plt.show()
+    # Irradiance plot (right y-axis for ax1)
+    ax1_twin = ax1.twinx()
+    ax1_twin.set_ylabel("Irradiance (W/m^2)", color=sun_color)
+    ax1_twin.plot(x, solar_energy, label="Irradiance", color=sun_color)
+    ax1_twin.tick_params(axis="y", labelcolor=sun_color)
+    ax1_twin.legend(loc="upper left")
+
+    # Other plot
+    ax2.plot(x, weather_df["Temperature"], label="Outside Air Temp", color=oat_color)
+    ax2.set_xlabel("Time")
+    ax2.set_ylabel("Temperature (°C)")
+    ax2.legend(loc="upper right")
+
+    plt.tight_layout()
+    plt.show()
 
 
+if __name__ == "__main__":
+    main()
