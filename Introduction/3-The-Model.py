1d7630c4d043cecf4cf451ab6a769dc8aeb0876d
Andrew Klavekoske <andrewklavo@gmail.com>
2024-07-26 00:39:28 -0600

    template for streamlit app

---
+++ Introduction/3-The-Model.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Thermal Simulation Report",
+    page_icon="☀️" 
+)
---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.logo("image_with_text.png")
+# Introduction Section
+motivation = st.Page("Introduction/1-Motivation.py", title="Motivation", icon=":material/target:")
+model_inputs = st.Page("Introduction/2-Model-Inputs.py", title="Model Inputs", icon=":material/input:")
+the_model = st.Page("Introduction/3-The-Model.py", title="The Model", icon=":material/aq_indoor:")
+the_sim = st.Page("Introduction/4-The-Simulation.py", title="The Simulation",  icon=":material/play_circle:")
+# Simulation Results Section
+no_flow_no_loss = st.Page("Simulation-Results/1-No-Flow-No-Loss.py", title="No Flow & No Losses", icon=":material/change_history:")
+no_flow = st.Page("Simulation-Results/2-No-Flow.py", title="No Flow", icon=":material/square:")
+contanst_flow = st.Page("Simulation-Results/3-Constant-Flow.py", title="Constant Flow", icon=":material/pentagon:")
+controlled_flow = st.Page("Simulation-Results/4-Controlled-Flow.py", title="Controlled Flow", icon=":material/hexagon:")
+
+pg = st.navigation(
+    {
+        "Introduction": [motivation, model_inputs, the_model, the_sim],
+        "Simulation Results": [no_flow_no_loss, no_flow, contanst_flow, controlled_flow],
+    }
+)
+
+pg.run()
