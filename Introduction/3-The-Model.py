9d03dd7b4b5703625d0c6395a77886f7f1ac76ae
Andrew Klavekoske <andrewklavo@gmail.com>
2024-07-27 15:24:22 -0600

    update show code sections

--- Introduction/3-The-Model.py
+++ Introduction/3-The-Model.py
@@ -38,223 +38,223 @@ show_code = st.toggle("Show Classes")
 if show_code:
     classes = '''
   # ------------------------------- Fluids -------------------------------
-  class Fluid:
-    def __init__(self,name: str, density: float, specific_heat: float, temperature: float,
-                  container=None, heat_transfer_coefficient=None):
-      self.name = name
-      self.density = density
-      self.specific_heat = specific_heat
-      self.temperature = temperature # initial temperature of water 20°C
-      self.container = container # container object if fluid is in a container
-      self.heat_transfer_coefficient = heat_transfer_coefficient
+class Fluid:
+  def __init__(self,name: str, density: float, specific_heat: float, temperature: float,
+                container=None, heat_transfer_coefficient=None):
+    self.name = name
+    self.density = density
+    self.specific_heat = specific_heat
+    self.temperature = temperature # initial temperature of water 20°C
+    self.container = container # container object if fluid is in a container
+    self.heat_transfer_coefficient = heat_transfer_coefficient
 
-    def get_name(self) -> str:
-      return self.name
+  def get_name(self) -> str:
+    return self.name
 
-    def volume(self) -> float:
-      return self.container.volume() if self.container else 0
+  def volume(self) -> float:
+    return self.container.volume() if self.container else 0
+  
+  def mass(self) -> float:
+    return self.density*self.volume()
+  
+  def temperature_in_kelvin(self) -> float:
+    return self.temperature + 273.15
+  
+  def add_container(self, container):
+    self.container = container
+  
+  # T2 = T1 + Q/mc
+  def add_energy(self, energy: float):
+    self.temperature += energy/(self.specific_heat*self.mass())
+
+  def lose_energy(self, energy: float):
+    self.temperature -= energy/(self.specific_heat*self.mass())
+
+  # heat lost by fluid_1 + Heat gained by fluid_2 = 0
+  def mix_with(self, fluid, flow_rate: float, time: float):
+    if flow_rate < 0:
+        raise ValueError("Flow rate must be non-negative.")
+    mass_1 = self.mass()
+    mass_2 = fluid.density*flow_rate*time
+    mass_3 = mass_1 + mass_2
+    temp_1 = self.temperature
+    temp_2 = fluid.temperature
+
+    # update both fluid temperature to final temperature
+    if flow_rate > 0:
+      self.temperature = ((mass_1*temp_1)+(mass_2*temp_2))/(mass_3) #update only temp in direction of flow
+      #fluid.temperature = self.temperature
+      return 
+
+# ------------------------------- Surfaces -------------------------------
+class Material:
+  def __init__(self, thermal_conductivity: float, surface_temperature: float, thickness: float):
+    self.thermal_conductivity = thermal_conductivity
+    self.surface_temperature = surface_temperature 
+    self.thickness = thickness
+
+# ------------------------------- The Sun -------------------------------
+class Sun:
+  def __init__(self):
+    self.irradiance = 0 # initial irradiance in W/m^2 or J/s*m^2
+
+  def energy(self, time: float, area: float) -> float:
+    return self.irradiance*time*area
+  
+# ------------------------------- Containers -------------------------------    
+class Container(ABC):
+  def __init__(self, fluid: Fluid, material: Material, surroundings: Fluid):
+    self.fluid = fluid
+    self.material = material
+    self.surroundings = surroundings # another fluid object (air)
+
+  @abstractmethod
+  def volume(self) -> float:
+    pass
+
+  @abstractmethod
+  def overall_UA(self) -> float:
+    pass
+
+  def temp_loss(self,temp_loss) -> float:
+    self.fluid.temperature -= temp_loss # used to simulate simple heat loss
+
+  def heat_loss(self, time) -> float:
+
+    heat_loss = ((self.fluid.temperature - self.surroundings.temperature) *
+                 self.overall_UA(self.surroundings) *
+                 time)
+
+    return heat_loss
+  
+  def radiation_loss(epsilon, sigma, A, T_surface, T_environment) -> float:
+      return epsilon * sigma * A * (T_surface**4 - T_environment**4) #not currently in use
+
+# ------------------------------- Solar Panel -------------------------------
+class SolarPanel(Container):
+  def __init__(self, fluid: Fluid, material: Material, surroundings: Fluid,
+                length: float, width: float, height: float):
+    super().__init__(fluid, material, surroundings)
+    self.length = length
+    self.width = width
+    self.height = height
+    self.efficiency = 0.8 # % of light energy converted to heat energy in water <-- this is a heat transfer band-Aid for now
+
+  def volume(self) -> float:
+    return self.length * self.width * self.height
+  
+  def surface_area(self) -> float:
+    top = (self.length * self.width) # assume bottom is lossless
+    left_right = 2*(self.length * self.height)
+    front_back = 2*(self.width * self.height)
+    return top + left_right + front_back
+  
+  def solar_area(self) -> float:
+    return self.length * self.width
+  
+  def overall_UA(self, air: Fluid) -> float:
+    r_inside = 1/self.fluid.heat_transfer_coefficient
+    r_material = self.material.thickness/self.material.thermal_conductivity
+    r_air = 1/air.heat_transfer_coefficient
+
+    overall_UA = (1/(r_inside + r_material  + r_air)) * self.surface_area()
+
+    return overall_UA
     
-    def mass(self) -> float:
-      return self.density*self.volume()
+# ------------------------------- Water Tank -------------------------------   
+class Tank(Container):
+  def __init__(self, fluid: Fluid, material: Material, surroundings: Fluid,
+                radius: float, height: float,  insulation: Material):
+    super().__init__(fluid, material, surroundings)
+    self.radius = radius
+    self.height = height
+    self.insulation = insulation
+
+  def volume(self) -> float:
+    return math.pi*self.height*self.radius**2
+  
+  def surface_area_walls(self, diameter) -> float:
+    return (math.pi*diameter*self.height)
+  
+  def surface_area_top(self) -> float:
+    return (math.pi*self.radius**2)
+  
+  #inner diameter of tank
+  def diameter_1(self) -> float:
+    return self.radius * 2
+  
+  #outer diameter of tank
+  def diameter_2(self) -> float:
+    return (self.radius + self.material.thickness) * 2
+  
+  #outer diameter of tank + insulation
+  def diameter_3(self) -> float:
+    return (self.radius + self.material.thickness + self.insulation.thickness) * 2
+  
+  # just tank walls
+  def overall_UA(self, air: Fluid) -> float:
+    # wall thermal resistances
+    # diameter ratios are necessary to correctly account for the cylindrical geometry and the logarithmic nature of radial heat conduction
+    wall_r_inside = 1/(self.surface_area_walls(self.diameter_1())*self.fluid.heat_transfer_coefficient)
+    wall_r_pipe = (math.log(self.diameter_2()/self.diameter_1()))/(self.material.thermal_conductivity*2*math.pi*self.height) 
+    wall_r_insulation = (math.log(self.diameter_3()/self.diameter_2()))/(self.insulation.thermal_conductivity*2*math.pi*self.height)
+    wall_r_air = 1/(air.heat_transfer_coefficient*self.surface_area_walls(self.diameter_3()))
+
+    # tank top thermal resistances
+    top_r_inside = 1/self.fluid.heat_transfer_coefficient
+    top_r_pipe = self.material.thickness/self.material.thermal_conductivity
+    top_r_insulation = self.insulation.thickness/self.insulation.thermal_conductivity
+    top_r_air = 1/air.heat_transfer_coefficient
+
+    wall_UA = 1/(wall_r_inside + wall_r_pipe + wall_r_insulation + wall_r_air)
+    top_UA = (1/(top_r_inside + top_r_pipe + top_r_insulation + top_r_air)) * self.surface_area_top()
+    overall_UA = wall_UA + top_UA
+
+    return overall_UA
     
-    def temperature_in_kelvin(self) -> float:
-      return self.temperature + 273.15
-    
-    def add_container(self, container):
-      self.container = container
-    
-    # T2 = T1 + Q/mc
-    def add_energy(self, energy: float):
-      self.temperature += energy/(self.specific_heat*self.mass())
+# ------------------------------- Pipe --------------------------------------   
+class Pipe(Container):
+  def __init__(self, fluid: Fluid, material: Material, surroundings: Fluid,
+                radius: float, length: float, insulation: Material):
+    super().__init__(fluid, material, surroundings)
+    self.radius = radius
+    self.length = length
+    self.insulation = insulation
 
-    def lose_energy(self, energy: float):
-      self.temperature -= energy/(self.specific_heat*self.mass())
-
-    # heat lost by fluid_1 + Heat gained by fluid_2 = 0
-    def mix_with(self, fluid, flow_rate: float, time: float):
-      if flow_rate < 0:
-          raise ValueError("Flow rate must be non-negative.")
-      mass_1 = self.mass()
-      mass_2 = fluid.density*flow_rate*time
-      mass_3 = mass_1 + mass_2
-      temp_1 = self.temperature
-      temp_2 = fluid.temperature
-
-      # update both fluid temperature to final temperature
-      if flow_rate > 0:
-        self.temperature = ((mass_1*temp_1)+(mass_2*temp_2))/(mass_3) #update only temp in direction of flow
-        #fluid.temperature = self.temperature
-        return 
-
-  # ------------------------------- Surfaces -------------------------------
-  class Material:
-    def __init__(self, thermal_conductivity: float, surface_temperature: float, thickness: float):
-      self.thermal_conductivity = thermal_conductivity
-      self.surface_temperature = surface_temperature 
-      self.thickness = thickness
-
-  # ------------------------------- The Sun -------------------------------
-  class Sun:
-    def __init__(self):
-      self.irradiance = 0 # initial irradiance in W/m^2 or J/s*m^2
-
-    def energy(self, time: float, area: float) -> float:
-      return self.irradiance*time*area
-    
-  # ------------------------------- Containers -------------------------------    
-  class Container(ABC):
-    def __init__(self, fluid: Fluid, material: Material, surroundings: Fluid):
-      self.fluid = fluid
-      self.material = material
-      self.surroundings = surroundings # another fluid object (air)
-
-    @abstractmethod
-    def volume(self) -> float:
-      pass
-
-    @abstractmethod
-    def overall_UA(self) -> float:
-      pass
-
-    def temp_loss(self,temp_loss) -> float:
-      self.fluid.temperature -= temp_loss # used to simulate simple heat loss
-
-    def heat_loss(self, time) -> float:
-
-      heat_loss = ((self.fluid.temperature - self.surroundings.temperature) *
-                  self.overall_UA(self.surroundings) *
-                  time)
-
-      return heat_loss
-    
-    def radiation_loss(epsilon, sigma, A, T_surface, T_environment) -> float:
-        return epsilon * sigma * A * (T_surface**4 - T_environment**4) #not currently in use
-
-  # ------------------------------- Solar Panel -------------------------------
-  class SolarPanel(Container):
-    def __init__(self, fluid: Fluid, material: Material, surroundings: Fluid,
-                  length: float, width: float, height: float):
-      super().__init__(fluid, material, surroundings)
-      self.length = length
-      self.width = width
-      self.height = height
-      self.efficiency = 0.8 # % of light energy converted to heat energy in water <-- this is a heat transfer Band-Aid for now
-
-    def volume(self) -> float:
-      return self.length * self.width * self.height
-    
-    def surface_area(self) -> float:
-      top = (self.length * self.width) # assume bottom is lossless
-      left_right = 2*(self.length * self.height)
-      front_back = 2*(self.width * self.height)
-      return top + left_right + front_back
-    
-    def solar_area(self) -> float:
-      return self.length * self.width
-    
-    def overall_UA(self, air: Fluid) -> float:
-      r_inside = 1/self.fluid.heat_transfer_coefficient
-      r_material = self.material.thickness/self.material.thermal_conductivity
-      r_air = 1/air.heat_transfer_coefficient
-
-      overall_UA = (1/(r_inside + r_material  + r_air)) * self.surface_area()
-
-      return overall_UA
-      
-  # ------------------------------- Water Tank -------------------------------   
-  class Tank(Container):
-    def __init__(self, fluid: Fluid, material: Material, surroundings: Fluid,
-                  radius: float, height: float,  insulation: Material):
-      super().__init__(fluid, material, surroundings)
-      self.radius = radius
-      self.height = height
-      self.insulation = insulation
-
-    def volume(self) -> float:
-      return math.pi*self.height*self.radius**2
-    
-    def surface_area_walls(self, diameter) -> float:
-      return (math.pi*diameter*self.height)
-    
-    def surface_area_top(self) -> float:
-      return (math.pi*self.radius**2)
-    
-    #inner diameter of tank
-    def diameter_1(self) -> float:
-      return self.radius * 2
-    
-    #outer diameter of tank
-    def diameter_2(self) -> float:
-      return (self.radius + self.material.thickness) * 2
-    
-    #outer diameter of tank + insulation
-    def diameter_3(self) -> float:
-      return (self.radius + self.material.thickness + self.insulation.thickness) * 2
-    
-    # just tank walls
-    def overall_UA(self, air: Fluid) -> float:
-      # wall thermal resistances
-      # diameter ratios are necessary to correctly account for the cylindrical geometry and the logarithmic nature of radial heat conduction
-      wall_r_inside = 1/(self.surface_area_walls(self.diameter_1())*self.fluid.heat_transfer_coefficient)
-      wall_r_pipe = (math.log(self.diameter_2()/self.diameter_1()))/(self.material.thermal_conductivity*2*math.pi*self.height) 
-      wall_r_insulation = (math.log(self.diameter_3()/self.diameter_2()))/(self.insulation.thermal_conductivity*2*math.pi*self.height)
-      wall_r_air = 1/(air.heat_transfer_coefficient*self.surface_area_walls(self.diameter_3()))
-
-      # tank top thermal resistances
-      top_r_inside = 1/self.fluid.heat_transfer_coefficient
-      top_r_pipe = self.material.thickness/self.material.thermal_conductivity
-      top_r_insulation = self.insulation.thickness/self.insulation.thermal_conductivity
-      top_r_air = 1/air.heat_transfer_coefficient
-
-      wall_UA = 1/(wall_r_inside + wall_r_pipe + wall_r_insulation + wall_r_air)
-      top_UA = (1/(top_r_inside + top_r_pipe + top_r_insulation + top_r_air)) * self.surface_area_top()
-      overall_UA = wall_UA + top_UA
-
-      return overall_UA
-      
-  # ------------------------------- Pipe --------------------------------------   
-  class Pipe(Container):
-    def __init__(self, fluid: Fluid, material: Material, surroundings: Fluid,
-                  radius: float, length: float, insulation: Material):
-      super().__init__(fluid, material, surroundings)
-      self.radius = radius
-      self.length = length
-      self.insulation = insulation
-
-    def volume(self) -> float:
-      return math.pi*self.length*self.radius**2
-    
-    def surface_area(self, diameter) -> float:
-      return (math.pi*diameter*self.length) # outer surface area
-    
-    #inner diameter of pipe
-    def diameter_1(self) -> float:
-      return self.radius * 2
-    
-    #outer diameter of pipe
-    def diameter_2(self) -> float:
-      return (self.radius + self.material.thickness) * 2
-    
-    #outer diameter of pipe + insulation
-    def diameter_3(self) -> float:
-      return (self.radius + self.material.thickness + self.insulation.thickness) * 2
+  def volume(self) -> float:
+    return math.pi*self.length*self.radius**2
+  
+  def surface_area(self, diameter) -> float:
+    return (math.pi*diameter*self.length) # outer surface area
+  
+  #inner diameter of pipe
+  def diameter_1(self) -> float:
+    return self.radius * 2
+  
+  #outer diameter of pipe
+  def diameter_2(self) -> float:
+    return (self.radius + self.material.thickness) * 2
+  
+  #outer diameter of pipe + insulation
+  def diameter_3(self) -> float:
+    return (self.radius + self.material.thickness + self.insulation.thickness) * 2
 
 
-    def overall_UA(self, air: Fluid) -> float:
-      # diameter ratios are necessary to correctly account for the cylindrical geometry and the logarithmic nature of radial heat conduction
-      r_inside = 1/(self.surface_area(self.diameter_1())*self.fluid.heat_transfer_coefficient)
-      r_pipe = (math.log(self.diameter_2()/self.diameter_1()))/(self.material.thermal_conductivity*2*math.pi*self.length) 
-      r_insulation = (math.log(self.diameter_3()/self.diameter_2()))/(self.insulation.thermal_conductivity*2*math.pi*self.length)
-      r_air = 1/(air.heat_transfer_coefficient*self.surface_area(self.diameter_3()))
+  def overall_UA(self, air: Fluid) -> float:
+    # diameter ratios are necessary to correctly account for the cylindrical geometry and the logarithmic nature of radial heat conduction
+    r_inside = 1/(self.surface_area(self.diameter_1())*self.fluid.heat_transfer_coefficient)
+    r_pipe = (math.log(self.diameter_2()/self.diameter_1()))/(self.material.thermal_conductivity*2*math.pi*self.length) 
+    r_insulation = (math.log(self.diameter_3()/self.diameter_2()))/(self.insulation.thermal_conductivity*2*math.pi*self.length)
+    r_air = 1/(air.heat_transfer_coefficient*self.surface_area(self.diameter_3()))
 
-      overall_UA = (1/(r_inside + r_pipe + r_insulation + r_air))
+    overall_UA = (1/(r_inside + r_pipe + r_insulation + r_air))
 
-      return overall_UA
-    
+    return overall_UA
+  
   # ------------------------------- Pump --------------------------------------
-  class Pump:
-    def __init__(self, flow_rate: float):
-      self.flow_rate = flow_rate
+class Pump:
+  def __init__(self, flow_rate: float):
+    self.flow_rate = flow_rate
    '''
     st.code(classes, language='python')
 
--- Introduction/4-The-Simulation.py
+++ Introduction/4-The-Simulation.py
@@ -40,46 +40,58 @@ if show_code:
         panel.fluid.add_energy(energy_to_panel)
 
         # Pump control
-        if supply_pipe.fluid.temperature < tank.fluid.temperature:
+        if pump_control == 0:
             pump.flow_rate = 0
-        else:
-            pump.flow_rate = 0.00063
+        elif pump_control == 1:
+            pump.flow_rate = flow_rate_initial
+        elif pump_control == 2:
+            if supply_pipe.fluid.temperature < tank.fluid.temperature:
+                pump.flow_rate = 0
+            else:
+                pump.flow_rate = 0.00063
 
         # Move and mix the fluids - This updates all fluid temps
         panel.fluid.mix_with(return_pipe.fluid, pump.flow_rate, sim_step_seconds)
         supply_pipe.fluid.mix_with(panel.fluid, pump.flow_rate, sim_step_seconds)
         tank.fluid.mix_with(supply_pipe.fluid, pump.flow_rate, sim_step_seconds)
         return_pipe.fluid.mix_with(tank.fluid, pump.flow_rate, sim_step_seconds)
-        
+      
         # Heat loss
         outside_air.temperature = weather_df.iloc[i]['Temperature']
+        if heat_loss:
+            panel_heat_loss = panel.heat_loss(sim_step_seconds)
+            supply_pipe_heat_loss = supply_pipe.heat_loss(sim_step_seconds)
+            tank_heat_loss = tank.heat_loss(sim_step_seconds)
+            return_pipe_heat_loss = return_pipe.heat_loss(sim_step_seconds)
 
-        panel_heat_loss = panel.heat_loss(sim_step_seconds)
-        supply_pipe_heat_loss = supply_pipe.heat_loss(sim_step_seconds)
-        tank_heat_loss = tank.heat_loss(sim_step_seconds)
-        return_pipe_heat_loss = return_pipe.heat_loss(sim_step_seconds)
-
-        panel.fluid.lose_energy(panel_heat_loss)    
-        supply_pipe.fluid.lose_energy(supply_pipe_heat_loss)
-        tank.fluid.lose_energy(tank_heat_loss)
-        return_pipe.fluid.lose_energy(return_pipe_heat_loss)
+            panel.fluid.lose_energy(panel_heat_loss)    
+            supply_pipe.fluid.lose_energy(supply_pipe_heat_loss)
+            tank.fluid.lose_energy(tank_heat_loss)
+            return_pipe.fluid.lose_energy(return_pipe_heat_loss)
+        else:
+            panel_heat_loss = 0
+            supply_pipe_heat_loss = 0
+            tank_heat_loss = 0
+            return_pipe_heat_loss = 0
 
         
         # store temperatures and energies and flows
         heat_transferred_to_air = (panel_heat_loss + supply_pipe_heat_loss +
                                     tank_heat_loss + return_pipe_heat_loss)
-        solar_energy.append(sun.irradiance)
-        panel_temperatures.append(panel.fluid.temperature)
-        supply_pipe_temperatures.append(supply_pipe.fluid.temperature)
-        tank_temperatures.append(tank.fluid.temperature)
-        return_pipe_temperatures.append(return_pipe.fluid.temperature)
-        zone_air_temps.append(zone_air.temperature)
-        panel_heat_losses.append(panel_heat_loss)
-        supply_pipe_heat_losses.append(supply_pipe_heat_loss)
-        tank_heat_losses.append(tank_heat_loss)
-        return_pipe_heat_losses.append(return_pipe_heat_loss)
-        total_heat_losses.append(heat_transferred_to_air)
-        flow_rates.append(pump.flow_rate)
+        sim_output_data['time'].append(weather_df.index[i])
+        sim_output_data['solar_energy'].append(sun.irradiance)
+        sim_output_data['panel_temperatures'].append(panel.fluid.temperature)
+        sim_output_data['supply_pipe_temperatures'].append(supply_pipe.fluid.temperature)
+        sim_output_data['tank_temperatures'].append(tank.fluid.temperature)
+        sim_output_data['return_pipe_temperatures'].append(return_pipe.fluid.temperature)
+        sim_output_data['zone_air_temperatures'].append(zone_air.temperature)
+        sim_output_data['outside_air_temperatures'].append(outside_air.temperature)
+        sim_output_data['panel_heat_losses'].append(panel_heat_loss)
+        sim_output_data['supply_pipe_heat_losses'].append(supply_pipe_heat_loss)
+        sim_output_data['tank_heat_losses'].append(tank_heat_loss)
+        sim_output_data['return_pipe_heat_losses'].append(return_pipe_heat_loss)
+        sim_output_data['total_heat_losses'].append(heat_transferred_to_air)
+        sim_output_data['flow_rates'].append(pump.flow_rate)
     print("Simulation complete!")
         '''
     st.code(sim_code, language='python')
