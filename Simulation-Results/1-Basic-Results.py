843251dc08a1d8b3d6525a2e697eb0525c17f3b5
Andrew Klavekoske <andrewklavo@gmail.com>
2024-07-28 12:02:34 -0600

    make plot a function in app.py and call it in basic results and simulation pages

--- Simulation-Results/1-Basic-Results.py
+++ Simulation-Results/1-Basic-Results.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import main
 import datetime
-import plotly.graph_objects as go
 import pandas as pd
-from plotly.subplots import make_subplots
+import app
+
+
 
 col1, col2, col3 = st.columns(3)
 
@@ -59,55 +60,7 @@ if start <= end:
         )
 
     results_df = pd.read_parquet("Outputs/thermal-simulation.parquet")
-    with st.spinner("Plotting results..."):
-        fig = make_subplots(rows=3, cols=1, shared_xaxes=True, vertical_spacing=0.05, specs=[[{"secondary_y": True}], [{"secondary_y": True}], [{"secondary_y": True}]], subplot_titles=("Weather", "Temperatures & Flow", "Heat Losses"))
-
-        # Weather plot
-        fig.add_trace(go.Scatter(x=results_df['Time'], y=results_df['Solar Energy'], name="Irradiance", line=dict(color="goldenrod")), row=1, col=1, secondary_y=False)
-        fig.add_trace(go.Scatter(x=results_df['Time'], y=results_df['Outside Air Temperatures'], name="Outside Air Temp", line=dict(color="green", dash="dash")), row=1, col=1, secondary_y=True)
-        fig.add_trace(go.Scatter(x=results_df['Time'], y=results_df['Zone Air Temperatures'], name="Zone Air Temp", line=dict(color="indigo", dash="dot")), row=1, col=1, secondary_y=True)
-
-        # Temperature plot
-        fig.add_trace(go.Scatter(x=results_df['Time'], y=results_df['Panel Temperatures'], name="Panel Fluid Temp", line=dict(color="firebrick")), row=2, col=1, secondary_y=False)
-        fig.add_trace(go.Scatter(x=results_df['Time'], y=results_df['Supply Pipe Temperatures'], name="Supply Pipe Fluid Temp", line=dict(color="chocolate", dash="dot")), row=2, col=1, secondary_y=False)
-        fig.add_trace(go.Scatter(x=results_df['Time'], y=results_df['Tank Temperatures'], name="Tank Fluid Temp", line=dict(color="orange")), row=2, col=1, secondary_y=False)
-        fig.add_trace(go.Scatter(x=results_df['Time'], y=results_df['Return Pipe Temperatures'], name="Return Pipe Fluid Temp", line=dict(color="blue", dash="dot")), row=2, col=1, secondary_y=False)
-        fig.add_trace(go.Scatter(x=results_df['Time'], y=results_df['Flow Rates'], name="Flow Rate", line=dict(color="purple"), opacity=0.5), row=2, col=1, secondary_y=True)
-    
-        # Heat Loss plot
-        fig.add_trace(go.Scatter(x=results_df['Time'], y=results_df['Supply Pipe Heat Losses'], name="Pipe Heat Loss", line=dict(color="chocolate", dash="dot")), row=3, col=1, secondary_y=False)
-        fig.add_trace(go.Scatter(x=results_df['Time'], y=results_df['Tank Heat Losses'], name="Tank Heat Loss", line=dict(color="orange")), row=3, col=1, secondary_y=False)
-        fig.add_trace(go.Scatter(x=results_df['Time'], y=results_df['Return Pipe Heat Losses'], name="Return Pipe Heat Loss", line=dict(color="blue", dash="dot")), row=3, col=1, secondary_y=False)
-        fig.add_trace(go.Scatter(x=results_df['Time'], y=results_df['Panel Heat Losses'], name="Panel Heat Loss", line=dict(color="firebrick")), row=3, col=1, secondary_y=True)
-        
-        # Update y-axes labels
-        fig.update_yaxes(title_text="Irradiance (W/m^2)", secondary_y=False, row=1, col=1, title_font=dict(color="goldenrod"), tickfont=dict(color="goldenrod"))
-        fig.update_yaxes(title_text="Temperature (°C)", secondary_y=True, row=1, col=1)
-        fig.update_yaxes(title_text="Temperature (°C)", secondary_y=False, row=2, col=1)
-        fig.update_yaxes(title_text="Flow Rate (m^3/s)", secondary_y=True, row=2, col=1, title_font=dict(color="purple"), tickfont=dict(color="purple"))
-        fig.update_yaxes(title_text="Heat Loss (J)", secondary_y=False, row=3, col=1)
-        fig.update_yaxes(title_text="Panel Heat Loss (J)", secondary_y=True, row=3, col=1, title_font=dict(color="firebrick"), tickfont=dict(color="firebrick"))        
-        
-        # Create separate legend groups
-        for i in range(1, 4):
-            for trace in fig.select_traces(row=i):
-                trace.update(legendgroup=f"group{i}")
-
-        # Position legends
-        fig.update_layout(
-            height=1000,
-            width=1200,
-            legend=dict(
-                orientation="v",
-                yanchor="top",
-                y=0.99,
-                xanchor="left",
-                x=1.1,
-                traceorder="grouped"
-            ),
-            legend_tracegroupgap=210
-        )
-
-        st.plotly_chart(fig, use_container_width=True)
+    fig = app.sim_output_plot(results_df)
+    st.plotly_chart(fig, use_container_width=True)
 else:
     st.error("Start date must be before or equal to end date.")
--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
+# st.set_page_config(
+#     page_title="Thermal Simulation Report",
+#     page_icon="☀️" 
+# )
 #st.logo("Images/image_with_text.png")
 # Introduction Section
 motivation = st.Page("Introduction/1-Motivation.py", title="Motivation", icon=":material/target:")
@@ -19,8 +25,56 @@ pg = st.navigation(
         "Simulation Results": [basic_results, regressions, sensitivity_analysis],
     }
 )
-st.set_page_config(
-    page_title="Thermal Simulation Report",
-    page_icon="☀️" 
-)
+
+
+def sim_output_plot(df):
+    with st.spinner("Plotting results..."):
+        fig = make_subplots(rows=3, cols=1, shared_xaxes=True, vertical_spacing=0.05, specs=[[{"secondary_y": True}], [{"secondary_y": True}], [{"secondary_y": True}]], subplot_titles=("Weather", "Temperatures & Flow", "Heat Losses"))
+
+        # Weather plot
+        fig.add_trace(go.Scatter(x=df['Time'], y=df['Solar Energy'], name="Irradiance", line=dict(color="goldenrod")), row=1, col=1, secondary_y=False)
+        fig.add_trace(go.Scatter(x=df['Time'], y=df['Outside Air Temperatures'], name="Outside Air Temp", line=dict(color="green", dash="dash")), row=1, col=1, secondary_y=True)
+        fig.add_trace(go.Scatter(x=df['Time'], y=df['Zone Air Temperatures'], name="Zone Air Temp", line=dict(color="indigo", dash="dot")), row=1, col=1, secondary_y=True)
+
+        # Temperature plot
+        fig.add_trace(go.Scatter(x=df['Time'], y=df['Panel Temperatures'], name="Panel Fluid Temp", line=dict(color="firebrick")), row=2, col=1, secondary_y=False)
+        fig.add_trace(go.Scatter(x=df['Time'], y=df['Supply Pipe Temperatures'], name="Supply Pipe Fluid Temp", line=dict(color="chocolate", dash="dot")), row=2, col=1, secondary_y=False)
+        fig.add_trace(go.Scatter(x=df['Time'], y=df['Tank Temperatures'], name="Tank Fluid Temp", line=dict(color="orange")), row=2, col=1, secondary_y=False)
+        fig.add_trace(go.Scatter(x=df['Time'], y=df['Return Pipe Temperatures'], name="Return Pipe Fluid Temp", line=dict(color="blue", dash="dot")), row=2, col=1, secondary_y=False)
+        fig.add_trace(go.Scatter(x=df['Time'], y=df['Flow Rates'], name="Flow Rate", line=dict(color="purple"), opacity=0.5), row=2, col=1, secondary_y=True)
+    
+        # Heat Loss plot
+        fig.add_trace(go.Scatter(x=df['Time'], y=df['Supply Pipe Heat Losses'], name="Pipe Heat Loss", line=dict(color="chocolate", dash="dot")), row=3, col=1, secondary_y=False)
+        fig.add_trace(go.Scatter(x=df['Time'], y=df['Tank Heat Losses'], name="Tank Heat Loss", line=dict(color="orange")), row=3, col=1, secondary_y=False)
+        fig.add_trace(go.Scatter(x=df['Time'], y=df['Return Pipe Heat Losses'], name="Return Pipe Heat Loss", line=dict(color="blue", dash="dot")), row=3, col=1, secondary_y=False)
+        fig.add_trace(go.Scatter(x=df['Time'], y=df['Panel Heat Losses'], name="Panel Heat Loss", line=dict(color="firebrick")), row=3, col=1, secondary_y=True)
+        
+        # Update y-axes labels
+        fig.update_yaxes(title_text="Irradiance (W/m^2)", secondary_y=False, row=1, col=1, title_font=dict(color="goldenrod"), tickfont=dict(color="goldenrod"))
+        fig.update_yaxes(title_text="Temperature (°C)", secondary_y=True, row=1, col=1)
+        fig.update_yaxes(title_text="Temperature (°C)", secondary_y=False, row=2, col=1)
+        fig.update_yaxes(title_text="Flow Rate (m^3/s)", secondary_y=True, row=2, col=1, title_font=dict(color="purple"), tickfont=dict(color="purple"))
+        fig.update_yaxes(title_text="Heat Loss (J)", secondary_y=False, row=3, col=1)
+        fig.update_yaxes(title_text="Panel Heat Loss (J)", secondary_y=True, row=3, col=1, title_font=dict(color="firebrick"), tickfont=dict(color="firebrick"))        
+        
+        # Create separate legend groups
+        for i in range(1, 4):
+            for trace in fig.select_traces(row=i):
+                trace.update(legendgroup=f"group{i}")
+
+        # Position legends
+        fig.update_layout(
+            height=1000,
+            width=1200,
+            legend=dict(
+                orientation="v",
+                yanchor="top",
+                y=0.99,
+                xanchor="left",
+                x=1.1,
+                traceorder="grouped"
+            ),
+            legend_tracegroupgap=210
+        )
+        return fig
 pg.run()
